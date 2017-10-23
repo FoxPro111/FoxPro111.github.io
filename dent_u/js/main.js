4d58ae36174c3d19b1a5962f01249d5fcf6ada70
Yuriy <yurec007@list.ru>
2017-10-23 23:26:30 +0300

    dent add
    + partners slider
    + text block
    + form

--- dent_u/js/main.js
+++ dent_u/js/main.js
@@ -13,17 +13,52 @@ jQuery(document).ready(function($) {
 	$('.menu__action').hover(
 	  function() {
 	  	$(this).addClass('hover');
+
 	  	$(this).find('.menu__link').css({
 	  		height: '60px'
 	  	});
-	  	$(this).css('margin-top', '-30px');
+	  	$(this).css('margin-top', '-30px').css('margin-bottom', '-30px');;
 	  }, function() {
 	  	$(this).removeClass('hover');
+
 	  	$(this).find('.menu__link').css({
 	  		height: '0'
 	  	});
-	  	$(this).css('margin-top', '0');
-
+	  	$(this).css('margin-top', '0').css('margin-bottom', '0');
 	  }
 	)
 });
+
+// partners slider
+
+$('.partner__slide').slick({
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	dots: true,
+	arrows: false,
+	swipe: false,
+	responsive: [{
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+				slidesToScroll: 3,
+        swipe: true
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+				slidesToScroll: 2,
+				dots: false,
+        swipe: true
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: false,
+        swipe: true
+      }
+    }]
+})
